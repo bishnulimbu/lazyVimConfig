3a1c93568fa178ec872dedd6828fe3634f777b1d
bishnulimbu <subashlimbu987654321@gmail.com>
2024-07-07 20:30:41 +0545

    added few keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
+
+--select all
+map("n", "<leader>9", "gg<S-V><S-G> <CR>", { desc = "select_all" })
+
+--will clear highlight
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
+
+-- save file
+map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
